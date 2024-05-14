b1366aa6e5dfbec76a82d570acec118617597f51
Miika Malin <miika.malin@oulu.fi>
2024-05-14 10:45:15 +0300

    Add output layer
    This layer is similar to to_input, but supports offset and rotation.
    Example usage:
    to_output('/path/to/mlp_output.png', rotation=180, axis="y", name="prediction", to='(final_layer-east)', off=2)

--- pycore/tikzeng.py
+++ pycore/tikzeng.py
@@ -40,6 +40,14 @@ def to_input( pathfile, to='(-3,0,0)', width=8, height=8, name="temp" ):
 \node[canvas is zy plane at x=0] (""" + name + """) at """+ to +""" {\includegraphics[width="""+ str(width)+"cm"+""",height="""+ str(height)+"cm"+"""]{"""+ pathfile +"""}};
 """
 
+def to_output(pathfile, off=0, to='(0,0,0)', width=8, height=8, name="temp", rotation=0, axis="x"):
+    off = -off
+    return r"""
+    \begin{scope}[rotate around """+ axis +"""="""+ str(rotation) +"""]
+    \\node[canvas is zy plane at x="""+ str(off) +"""] (""" + name + """) at """+ to +""" {\includegraphics[width="""+ str(width)+"cm"+""",height="""+ str(height)+"cm"+"""]{"""+ pathfile +"""}};
+    \end{scope}
+    """
+
 # Conv
 def to_Conv( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=40, caption=" " ):
     return r"""
